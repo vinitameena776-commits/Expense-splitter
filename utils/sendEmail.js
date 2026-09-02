bfb4fe6377b7ce056d702e6d419fdc8b1ce99522
Vinita Meena <vinitameena776@gmail.com>
2026-09-02 18:35:56 +0530

    Update Gmail SMTP configuration

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,9 +2,10 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+host: 'smtp.gmail.com',
+port: 587,
+secure: false,
+requireTLS: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
