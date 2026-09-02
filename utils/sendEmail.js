056cdf746b31ffa98aea9026cc84fa37248c4e1a
Vinita Meena <vinitameena776@gmail.com>
2026-09-02 18:28:01 +0530

    Fix Gmail SMTP connection

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,11 +2,16 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
   });
 
   const mailOptions = {
